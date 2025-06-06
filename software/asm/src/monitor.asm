05d969aa1dbb8c65c410c3e38a4986b266ea0fe3
aaronsbrown <aaron.s.brown@gmail.com>
2025-06-06 14:46:49 -0700

    monitor_WIP

--- software/asm/src/monitor.asm
+++ software/asm/src/monitor.asm
@@ -4,9 +4,9 @@ INCLUDE "includes/mmio_defs.inc"
 ; ======================================================================
 ; CONSTANTS
 ; ======================================================================
-ASCII_O     EQU $4F
-ASCII_K     EQU $4B
-ASCII_LF    EQU $0A
+SMC_LDA_ADDR    EQU $0200       ; beginning of general RAM
+SMC_LDA_OPCODE  EQU $A0         ; opcode for LDA (load A with Mem contents) instruction
+SMC_RET_OPCODE  EQU $19         ; opcode for RET (return) instruction
 
 
 ; ======================================================================
@@ -17,18 +17,28 @@ ASCII_LF    EQU $0A
 
 START:
 
-    LDI A, #ASCII_O
-    JSR SEND_BYTE
-
-    LDI A, #ASCII_K
-    JSR SEND_BYTE
-
-    LDI A, #ASCII_LF
-    JSR SEND_BYTE
-
+    JSR INIT
+    JSR PRINT_WELCOME_MSG
     JSR MAIN_LOOP
 
     HLT
+
+
+; ======================================================================
+; == SUBROUTINE: INIT
+; ======================================================================
+INIT:
+    
+    LDI A, #SMC_LDA_OPCODE      ; load LDA op code to self-modifying code RAM block
+    STA SMC_LDA_ADDR
+    
+    LDI A, #SMC_RET_OPCODE      ; load RET op code to self-modifying code RAM block
+    STA SMC_LDA_ADDR + 3 
+    
+    welcome_message: DB "Hello!\n", 0           ; 48, 65, 6C, 6C, 6F, 21, 0A, 00
+    welcome_message_addr: DW welcome_message 
+    
+    RET
 
 
 ; ======================================================================
@@ -37,8 +47,37 @@ START:
 MAIN_LOOP:
     JSR RECEIVE_BYTE
     JSR SEND_BYTE
-    JMP MAIN_LOOP
+    JMP MAIN_LOOP               ; infinite loop
 
+
+; ======================================================================
+; == SUBROUTINE: PRINT_WELCOME_MSG
+; ======================================================================
+PRINT_WELCOME_MSG:
+    
+    LDI B, #LOW_BYTE(welcome_message_addr)
+
+.loop
+    ; load welcome_message_addr low byte into SMC_LDA_ADDR + 1
+    MOV B, A
+    STA SMC_LDA_ADDR + 1
+
+    ; load welcome_message_addr high byte into SMC_LDA_ADDR + 2
+    LDI A, #HIGH_BYTE(welcome_message_addr)
+    STA SMC_LDA_ADDR + 2
+
+    JSR SMC_LDA_ADDR        ; load A with (char) byte
+    JSR SEND_BYTE           ; print byte
+
+    LDI C, #$00             ; C = $00 (null terminator)
+    CMP C                   ; if A = C
+    JZ .return             ; return
+
+    INR B ; icnrement B     ; else, increment B
+    JMP .loop
+
+.return:
+    RET
 
 ; ======================================================================
 ; == SUBROUTINE: RECEIVE_BYTE
@@ -58,7 +97,7 @@ POLL_RX_READY:
 ; == SUBROUTINE: SEND_BYTE
 ; ======================================================================
 SEND_BYTE:
-    MOV A, B                            ; save A in B
+    PHA                                 ; save A to stack
 
 POLL_TX_BUFFER:
 
@@ -66,6 +105,6 @@ POLL_TX_BUFFER:
     ANI #MASK_TX_BUFFER_EMPTY           ; isolate buffer_empty bit
     JZ POLL_TX_BUFFER                   ; buffer_empty == 0 ? poll : send_BYTE
     
-    MOV B, A                            ; reload A
+    PLA                                 ; reload A from stack
     STA UART_DATA_REG                   ; send A
     RET
