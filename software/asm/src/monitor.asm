2d7a117005684beeded6b801924b8d25a872c707
aaronsbrown <aaron.s.brown@gmail.com>
2025-06-05 18:48:07 -0700

    add echo loop to monitor

--- software/asm/src/monitor.asm
+++ software/asm/src/monitor.asm
@@ -6,6 +6,7 @@ INCLUDE "includes/mmio_defs.inc"
 ; ======================================================================
 ASCII_O     EQU $4F
 ASCII_K     EQU $4B
+ASCII_LF    EQU $0A
 
 
 ; ======================================================================
@@ -15,25 +16,55 @@ ASCII_K     EQU $4B
 
 
 START:
+
     LDI A, #ASCII_O
-    JSR SEND_CHAR
+    JSR SEND_BYTE
 
     LDI A, #ASCII_K
-    JSR SEND_CHAR
+    JSR SEND_BYTE
+
+    LDI A, #ASCII_LF
+    JSR SEND_BYTE
+
+    JSR MAIN_LOOP
 
     HLT
 
+
 ; ======================================================================
-; == SUBROUTINE: SEND_CHAR
+; == SUBROUTINE:MAIN_LOOP
 ; ======================================================================
-SEND_CHAR:
+MAIN_LOOP:
+    JSR RECEIVE_BYTE
+    JSR SEND_BYTE
+    JMP MAIN_LOOP
+
+
+; ======================================================================
+; == SUBROUTINE: RECEIVE_BYTE
+; ======================================================================
+RECEIVE_BYTE:
+
+POLL_RX_READY:
+    LDA UART_STATUS_REG
+    ANI #MASK_RX_DATA_READY
+    JZ POLL_RX_READY
+
+    LDA UART_DATA_REG
+    RET
+
+
+; ======================================================================
+; == SUBROUTINE: SEND_BYTE
+; ======================================================================
+SEND_BYTE:
     MOV A, B                            ; save A in B
 
 POLL_TX_BUFFER:
 
     LDA UART_STATUS_REG                 ; load uart status reg
     ANI #MASK_TX_BUFFER_EMPTY           ; isolate buffer_empty bit
-    JZ POLL_TX_BUFFER                   ; buffer_empty == 0 ? poll : send_char
+    JZ POLL_TX_BUFFER                   ; buffer_empty == 0 ? poll : send_BYTE
     
     MOV B, A                            ; reload A
     STA UART_DATA_REG                   ; send A
