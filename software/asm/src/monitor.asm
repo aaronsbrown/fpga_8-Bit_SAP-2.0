f0669a5fe55a806cdf5ef48197a934ec2b69c06b
aaronsbrown <aaron.s.brown@gmail.com>
2025-06-06 17:15:34 -0700

    monitor: print_string routine

--- software/asm/src/monitor.asm
+++ software/asm/src/monitor.asm
@@ -19,6 +19,7 @@ START:
 
     JSR INIT
     JSR PRINT_WELCOME_MSG
+    JSR PRINT_WG_MSG
     JSR MAIN_LOOP
 
     HLT
@@ -35,11 +36,15 @@ INIT:
     LDI A, #SMC_RET_OPCODE      ; load RET op code to self-modifying code RAM block
     STA SMC_LDA_ADDR + 3 
     
-    welcome_message: DB "Hello!\n", 0           ; 48, 65, 6C, 6C, 6F, 21, 0A, 00
-    welcome_message_addr: DW welcome_message 
-    
     RET
-
+    
+    
+; ======================================================================
+; == DATA
+; ======================================================================
+welcome_message: DB "ASB Monitor v0.1>\n", 0           
+wg_message: DB "Shall we play a game?\n", 0
+    
 
 ; ======================================================================
 ; == SUBROUTINE:MAIN_LOOP
@@ -54,30 +59,56 @@ MAIN_LOOP:
 ; == SUBROUTINE: PRINT_WELCOME_MSG
 ; ======================================================================
 PRINT_WELCOME_MSG:
-    
-    LDI B, #LOW_BYTE(welcome_message_addr)
 
-.loop
-    ; load welcome_message_addr low byte into SMC_LDA_ADDR + 1
-    MOV B, A
-    STA SMC_LDA_ADDR + 1
-
-    ; load welcome_message_addr high byte into SMC_LDA_ADDR + 2
-    LDI A, #HIGH_BYTE(welcome_message_addr)
-    STA SMC_LDA_ADDR + 2
-
-    JSR SMC_LDA_ADDR        ; load A with (char) byte
-    JSR SEND_BYTE           ; print byte
-
-    LDI C, #$00             ; C = $00 (null terminator)
-    CMP C                   ; if A = C
-    JZ .return             ; return
-
-    INR B ; icnrement B     ; else, increment B
-    JMP .loop
-
-.return:
+    LDI C, #LOW_BYTE(welcome_message)       ; C = low bytes of string start address
+    LDI B, #HIGH_BYTE(welcome_message)      ; B = high bytes of string start address
+    JSR PRINT_STRING
     RET
+
+
+; ======================================================================
+; == SUBROUTINE: PRINT_WG_MSG
+; ======================================================================
+PRINT_WG_MSG:
+
+    LDI C, #LOW_BYTE(wg_message)       ; C = low bytes of string start address
+    LDI B, #HIGH_BYTE(wg_message)      ; B = high bytes of string start address
+    JSR PRINT_STRING
+    RET
+
+
+
+; ======================================================================
+; == SUBROUTINE: PRINT_STRING
+; ======================================================================
+PRINT_STRING:
+
+.loop:
+
+    MOV C, A                         
+    STA (SMC_LDA_ADDR + 1)          ; Store low byte at self-modifying code operand address + 1
+
+    MOV B, A
+    STA (SMC_LDA_ADDR + 2)          ; Store high byte at self-modifying code operand address + 2
+
+    ; ====== A_SAFE ====== 
+    JSR SMC_LDA_ADDR                ; ***SETS A*** execute self-modifying code op: A = LDA[BC]
+    
+    ORI #0                          ; A | 0 = 0? 
+    JZ .finished                    ; if Z = 1, we're finished
+
+    JSR SEND_BYTE                   ; assumed A = byte to send
+    ; ==== END A_SAFE ====
+
+    INR C                           ; increment LOW BYTE of string pointer
+    JNZ .loop                       ; if not, loop
+    INR B                           ; if so, increment B
+    JMP .loop                       ; loop
+
+
+.finished:
+    RET
+
 
 ; ======================================================================
 ; == SUBROUTINE: RECEIVE_BYTE
